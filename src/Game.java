7928f56f2f67f73f128fbef72d48f6644639d39d
migwell-dev <miggyfortuna0413@gmail.com>
2023-08-18 21:59:55 +0800

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+public class Game {
+    Scanner sc = new Scanner(System.in);
+    private int[] array;
+    private String name;
+    private int spot;
+    private int num;
+
+    public Game(String playerName) {
+        this.array = new int[10];
+        for (int i : this.array) {
+            array[i] = 0;
+        }
+        this.name = playerName;
+    }
+
+    public void generateNum() {
+        this.num = (int) (Math.random() * 1000);
+    }
+
+    public void setSpot() {
+        System.out.println("Pick an empty spot to place your number. 1-10 only.");
+        this.spot = sc.nextInt() - 1;
+    }
+    public void insertNum() {
+        this.array[this.spot] = this.num;
+    }
+
+    public void numPlace() {
+        boolean placed = false;
+        while (!placed) {
+            setSpot();
+            if (array[this.spot] != 0) {
+                System.out.println("Spot is already occupied. Choose another.");
+            } else {
+                insertNum();
+                placed = true;
+            }
+        }
+    }
+
+    public boolean validMove() {
+        if ((this.spot != 0 && this.array[this.spot - 1] != 0 && this.num < this.array[this.spot - 1])
+                || (this.spot != this.array.length-1 && this.array[this.spot + 1] != 0 && this.num > this.array[this.spot + 1])) {
+            return false;
+        }
+        return true;
+    }
+
+    public void win() {
+        System.out.println("You won the game! All of the numbers are in ascending order!");
+        printCurrentList();
+    }
+
+    public void printCurrentList() {
+        for (int i = 0; i < this.array.length; i++) {
+            if (this.array[i] == 0) {
+                System.out.println((i+1) + ". ");
+            } else {
+                System.out.println((i+1) + ". " + this.array[i]);
+            }
+        }
+    }
+
+    public void playGame() {
+        System.out.println("The rules are simple: place the numbers in ascending order! Try not to misplace any...");
+        System.out.println("Game has started! Good luck, " + this.name);
+
+        for(int i = 0; i < this.array.length; i++) {
+            generateNum();
+            System.out.printf("The number is %d\n", this.num);
+            printCurrentList();
+            numPlace();
+            if (!validMove()) {
+                System.out.println("Game over!");
+                break;
+            }
+        }
+        win();
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Game myGame = new Game("Miguel");
+        myGame.playGame();
+    }
+}
